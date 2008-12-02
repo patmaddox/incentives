d2e23857aa6a818e8e4cd8ee1e041b0a440b73e3
Pat Maddox <pat.maddox@gmail.com>
2008-12-02 09:48:45 -0800

    made containing_item composable

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,7 @@ class Cart < ActiveRecord::Base
   has_many :items, :through => :cart_items
   
   named_scope(:total_greater_than, lambda {|total| {:conditions => ['total > ?', total]}})
-  named_scope(:containing_item, lambda {|item| {:conditions => ['cart_items.item_id=?', item], :joins => :cart_items}})
+  named_scope(:containing_item, lambda {|item| {:conditions => ['SELECT COUNT(*) FROM cart_items WHERE cart_items.cart_id=carts.id AND cart_items.item_id=?', item]}})
   
   def purchase(amount)
     update_attribute :total, total.to_i + amount
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -34,12 +34,26 @@ describe Cart do
     Cart.total_greater_than(75).should_not include(less_than)
   end
 
-  it "containing_item should find carts that contain the item" do
-    item = Item.create!
-    with_item = Cart.create!
-    with_item.add_to_cart item
-    without_item = Cart.create!
-    Cart.containing_item(item).should include(with_item)
-    Cart.containing_item(item).should_not include(without_item)
+  describe "containing_item" do
+    it "should find carts that contain the item" do
+      item = Item.create!
+      with_item = Cart.create!
+      with_item.add_to_cart item
+      without_item = Cart.create!
+      Cart.containing_item(item).should include(with_item)
+      Cart.containing_item(item).should_not include(without_item)
+    end
+
+    it "should be composable with itself" do
+      item1 = Item.create!
+      item2 = Item.create!
+      with_both = Cart.create!
+      with_both.add_to_cart item1
+      with_both.add_to_cart item2
+      with_one = Cart.create!
+      with_one.add_to_cart item1
+      Cart.containing_item(item1).containing_item(item2).should include(with_both)
+      Cart.containing_item(item1).containing_item(item2).should_not include(with_one)
+    end
   end
 end
